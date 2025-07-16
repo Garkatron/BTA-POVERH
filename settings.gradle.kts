b6cb1a297b3806566bed6922dec4d46c52bad13f
Garkatron <145294535+Garkatron@users.noreply.github.com>
2025-07-17 01:03:02 +0200

    Setup 0 again

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,21 @@
 pluginManagement {
 	repositories {
 		gradlePluginPortal()
-		maven { url = uri("https://maven.fabricmc.net/") }
-		maven { url = uri("https://jitpack.io") }
-		maven { url = uri("https://maven.glass-launcher.net/babric") }
-		maven { url = uri("https://maven.thesignalumproject.net/infrastructure") }
-	}
-	plugins {
-		id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-		id("fabric-loom") version "1.10.0-bta"
-		id("com.modrinth.minotaur") version "2.+"
-		id("org.jetbrains.kotlin.jvm") version "2.1.0"
+		maven {
+			name = "Fabric"
+			url = uri("https://maven.fabricmc.net/")
+		}
+		maven {
+			name = "Jitpack"
+			url = uri("https://jitpack.io")
+		}
+		maven {
+			name = "Babric"
+			url = uri("https://maven.glass-launcher.net/babric")
+		}
+		maven {
+			name = "SignalumMavenInfrastructure"
+			url = uri("https://maven.thesignalumproject.net/infrastructure")
+		}
 	}
 }
-rootProject.name = "bta-poverh"
-
-include("momentum")
-include("parcool")
